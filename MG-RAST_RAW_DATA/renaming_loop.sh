56a63d12f0d831c6562f7f619ba83ad9277f98a7
joynerco22 <112597907+joynerco22@users.noreply.github.com>
2024-02-27 13:27:09 -0500

    Update renaming_loop.sh

--- MG-RAST_RAW_DATA/renaming_loop.sh
+++ MG-RAST_RAW_DATA/renaming_loop.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# Find all project directories (replace with your actual directory pattern)
+# Find all project directories. Modify this depending on your own directory requirements. In this case, the downloaded files exist in directories all beginning with mgp* followed by a numeric key. 
 project_directories=$(ls -d ~/MG-RAST_RAW_DATA/mgp*/)
 
 
-# Loop through project directories
+# Loop through project directories, then cut the numeric project ID off and save it as a variable. We're gonna use this to append to any downloaded metagenomes, to keep track of which project they originated.
 for project_dir in $project_directories; do
   # Extract the unique identifier (e.g., "mgp100537" to "100537")
   identifier=$(echo "$project_dir" | sed 's/.*mgp\([0-9]*\).*/\1/')
 
-  # Find .fna files and rename them to the desired format
-  find "$project_dir" -type f -name "*.*.fastq" | while IFS= read -r fna_file; do
+  # Find .fna files and rename them to the desired format. Note that you'll want to modify the -name flag to either .fna or .fastq depending on your file requirements!
+  find "$project_dir" -type f -name "*.*.fna" | while IFS= read -r fna_file; do
     base_filename=$(basename "$fna_file")
 
     # Remove the ".screen.passed" from the filename and append the correct identifier
